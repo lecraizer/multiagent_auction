33629e6012e47964a0eae0aef7cbd8d94aacc3ab
Fernanda Gomes <fernandalsgomes06@gmail.com>
2025-05-13 16:42:12 -0300

    removed check_actor_params method

--- refactoring/agent.py
+++ refactoring/agent.py
@@ -1,44 +1,91 @@
-import random
 import numpy as np
 import torch as T
 from networks import ActorNetwork, CriticNetwork
 
 class Agent(object):
-    def __init__(self, alpha, beta, input_dims, tau, gamma=0.99,
-                 n_agents=2, n_actions=1, layer1_size=400, layer2_size=300, 
-                 batch_size=64, total_eps=100000, noise_std=0.2, 
-                 tl_flag=False, extra_players=0):
+    """
+    Agent implementation with actor-critic architecture.
+    """
+    def __init__(self, 
+                 alpha: float, 
+                 beta: float, 
+                 input_dims: int, 
+                 tau: float, 
+                 gamma: float = 0.99, 
+                 n_agents: int = 2, 
+                 n_actions: int = 1, 
+                 layer1_size: int = 400, 
+                 layer2_size: int = 300, 
+                 batch_size: int = 64, 
+                 total_eps: int = 100000, 
+                 noise_std: float = 0.2, 
+                 tl_flag: bool = False, 
+                 extra_players: int = 0):
+        """
+        Initialize the agent with actor and critic networks.
+        
+        Args:
+            alpha (float): Learning rate for the actor network.
+            beta (float): Learning rate for the critic network.
+            input_dims (int): Dimensionality of the input state space.
+            tau (float): Update parameter for target network updates.
+            gamma (float, optional): Discount factor for future rewards. Defaults is 0.99.
+            n_agents (int, optional): Number of agents in the environment. Defaults is 2.
+            n_actions (int, optional): Dimensionality of the action space. Defaults is 1.
+            layer1_size (int, optional): Size of the first hidden layer in networks. Defaults is 400.
+            layer2_size (int, optional): Size of the second hidden layer in networks. Defaults is 300.
+            batch_size (int, optional): Size of batches for training. Defaults is 64.
+            total_eps (int, optional): Total number of episodes for training. Defaults is 100000.
+            noise_std (float, optional): Standard deviation of exploration noise. Defaults is 0.2.
+            tl_flag (bool, optional): Flag to enable transfer learning. Defaults is False.
+            extra_players (int, optional): Number of additional players for transfer learning. Defaults is 0.
+        """
         self.gamma = gamma
         self.tau = tau
         self.batch_size = batch_size
         self.total_episodes = total_eps
         self.noise_std = noise_std
-        self.actor = ActorNetwork(alpha, input_dims, layer1_size,
-                                  layer2_size, n_actions=n_actions,
+        self.actor = ActorNetwork(alpha, input_dims, layer1_size, layer2_size, n_actions=n_actions, 
                                   name='actor', n_agents=n_agents)
 
-        self.critic = CriticNetwork(beta, input_dims, layer1_size,
-                                    layer2_size, n_actions=n_actions,
-                                    name='critic', n_agents=n_agents,
-                                    flag=tl_flag, extra=extra_players)
+        self.critic = CriticNetwork(beta, input_dims, layer1_size, layer2_size, n_actions=n_actions, 
+                                    name='critic', n_agents=n_agents, flag=tl_flag, extra=extra_players)
 
-        self.target_actor = ActorNetwork(alpha, input_dims, layer1_size,
-                                         layer2_size, n_actions=n_actions,
+        self.target_actor = ActorNetwork(alpha, input_dims, layer1_size, layer2_size, n_actions=n_actions, 
                                          name='target_actor', n_agents=n_agents)
-        self.target_critic = CriticNetwork(beta, input_dims, layer1_size,
-                                           layer2_size, n_actions=n_actions,
-                                           name='target_critic', n_agents=n_agents,
-                                           flag=tl_flag, extra=extra_players)
+        
+        self.target_critic = CriticNetwork(beta, input_dims, layer1_size, layer2_size, n_actions=n_actions,
+                                           name='target_critic', n_agents=n_agents, flag=tl_flag, extra=extra_players)
 
         self.update_network_parameters(tau=1)
+
+    def get_networks_dicts(self):
+        """
+        Get state dictionaries for all networks.
+        
+        Returns:
+            tuple: Four dictionaries containing the named parameters of the critic,
+                  actor, target critic, and target actor networks respectively.
+        """
+        critic_state_dict = dict(self.critic.named_parameters())
+        actor_state_dict = dict(self.actor.named_parameters())
+        target_critic_dict = dict(self.target_critic.named_parameters())
+        target_actor_dict = dict(self.target_actor.named_parameters())
+
+        return critic_state_dict, actor_state_dict, target_critic_dict, target_actor_dict
     
-        # from torchviz import make_dot 
-        # dot = make_dot(self.actor(T.randn(400, 1)), params=dict(self.actor.named_parameters()))
-        # dot.format = 'png'
-        # dot.render('actor', format='png')
-
-
-    def choose_action(self, observation, episode, evaluation=False):
+    def choose_action(self, observation, episode, evaluation = False):
+        """
+        Select an action based on the current policy and exploration strategy.
+        
+        Args:
+            observation: The current state of the environment.
+            episode (int): Current episode number, used for noise decay.
+            evaluation (bool, optional): If True, no exploration noise is added. Defaults is False.
+            
+        Returns:
+            numpy.ndarray: The selected action.
+        """
         self.actor.eval()
         observation = T.tensor([observation], dtype=T.float).to(self.actor.device)
         mu = self.actor.forward(observation).to(self.actor.device)
@@ -50,67 +97,44 @@ class Agent(object):
             mu_prime = mu + (noise*(1-(episode/self.total_episodes)))
             mu_prime = mu_prime.clamp(0, 1)
 
-            # # agent has chance = exploration_rate to play randomly 
-            # exploration_rate = 1-(episode/self.total_episodes)
-            # if random.random() < exploration_rate:
-            #     mu_prime = T.tensor([np.random.uniform(0, 1)], dtype=T.float).to(self.actor.device)
-            # else:
-            #     mu_prime = mu
-
-            # mu_prime = mu_prime.clamp(0, 1)
-        
-            # mu_prime = mu.clamp(0, 1)
-
         self.actor.train()
         return mu_prime.cpu().detach().numpy()
 
     def update_network_parameters(self, tau=None):
-        if tau is None:
-            tau = self.tau
-
-        actor_params = self.actor.named_parameters()
-        critic_params = self.critic.named_parameters()
-        target_actor_params = self.target_actor.named_parameters()
-        target_critic_params = self.target_critic.named_parameters()
-
-        critic_state_dict = dict(critic_params)
-        actor_state_dict = dict(actor_params)
-        target_critic_dict = dict(target_critic_params)
-        target_actor_dict = dict(target_actor_params)
+        """
+        Update target network parameters.
+        
+        Args:
+            tau (float, optional): Update parameter. If None, use the
+                                  instance's tau value. Defaults is None.
+        """
+        if tau is None: tau = self.tau
+        critic_state_dict, actor_state_dict, target_critic_dict, target_actor_dict = self.get_networks_dicts()
 
         for name in critic_state_dict:
             critic_state_dict[name] = tau*critic_state_dict[name].clone() + (1-tau)*target_critic_dict[name].clone()
-
-        self.target_critic.load_state_dict(critic_state_dict)
-
         for name in actor_state_dict:
             actor_state_dict[name] = tau*actor_state_dict[name].clone() + (1-tau)*target_actor_dict[name].clone()
+
+        self.target_critic.load_state_dict(critic_state_dict)
         self.target_actor.load_state_dict(actor_state_dict)
         
     def save_models(self, name):
-        self.actor.save_checkpoint(name)
-        self.target_actor.save_checkpoint(name)
-        self.critic.save_checkpoint(name)
-        self.target_critic.save_checkpoint(name)
+        """
+        Save all network models to checkpoint files.
+        
+        Args:
+            name (str): Identifier to append to the checkpoint filenames.
+        """
+        for network in (self.actor, self.target_actor, self.critic, self.target_critic):
+            network.save_checkpoint(name)
 
     def load_models(self, name):
-        self.actor.load_checkpoint(name)
-        self.target_actor.load_checkpoint(name)
-        self.critic.load_checkpoint(name)
-        self.target_critic.load_checkpoint(name)
-
-    def check_actor_params(self):
-        current_actor_params = self.actor.named_parameters()
-        current_actor_dict = dict(current_actor_params)
-        original_actor_dict = dict(self.original_actor.named_parameters())
-        original_critic_dict = dict(self.original_critic.named_parameters())
-        current_critic_params = self.critic.named_parameters()
-        current_critic_dict = dict(current_critic_params)
-        print('Checking Actor parameters')
-
-        for param in current_actor_dict:
-            print(param, T.equal(original_actor_dict[param], current_actor_dict[param]))
-        print('Checking critic parameters')
-        for param in current_critic_dict:
-            print(param, T.equal(original_critic_dict[param], current_critic_dict[param]))
-        input()
+        """
+        Load all network models from checkpoint files.
+        
+        Args:
+            name (str): Identifier of the checkpoint files to load.
+        """
+        for network in (self.actor, self.target_actor, self.critic, self.target_critic):
+            network.load_checkpoint(name)
